50005b184b9c773209c83acf6436ccc88c8300fd
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-23 18:37:27 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -64,22 +64,23 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  new_movie = Movie.find_by(title: "Awesome Flick")
+  new_movie.update(title:"Even Awesomer Flick")
+  new_movie.save
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  new_movie = Movie.find_by(title: "Wat?")
+  new_movie.update(title: "Wat, huh?")
+  #new_movie.save
 end
 
 def can_update_multiple_items_at_once
@@ -87,18 +88,22 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.all.each do |movie|
+    movie.update(title: "A Movie")
+  end
 end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  new_movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  new_movie.delete
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.all.each do |movie|
+    movie.delete
+  end
 end
