810a0547a02fe5334b643f41592e16d664d22c0f
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-23 18:31:30 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,7 +79,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   new_movie = Movie.find_by(title: "Wat, huh?")
-  new_movie.save
+  #new_movie.save
 end
 
 def can_update_multiple_items_at_once
